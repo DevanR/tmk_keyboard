fce0104a8aa7f299073757629333de1da4c3fd10
Rehunathan, Devan <devan.rehunathan@bskyb.com>
2017-02-08 13:19:00 +0000

    New layout

--- keyboard/gh60/keymap_devan.c
+++ keyboard/gh60/keymap_devan.c
@@ -38,43 +38,43 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* 2: FN 0
      * ,---------------------------------------------------------------------------.
-     * |ESC   |RST|PRINT|  POWER|  SLEEP|  WAKE|  1|   2|     3|     0|  +|    Bspc|
+     * |ESC   |RST|PRINT|  POWER|  SLEEP|  WAKE|  /|   0|     1|     2|  3|    Bspc|
      * |---------------------------------------------------------------------------|
-     * |TAB   |PLAY|STOP|  EJECT|  PREV |  NEXT|  4|   5|     6|     -|  *|  Return|
+     * |TAB   |PLAY|STOP|  EJECT|  PREV |  NEXT|  =|   -|     4|     5|  6|  Return|
      * |---------------------------------------------------------------------------|
-     * |      |  QW|  DV|   MUTE|  VOLU |  VOLD|  7|   8|     9|     /|  =|        |
+     * |      |  QW|  DV|   MUTE|  VOLU |  VOLD|  *|   +|     7|     8|  9|        |
      * |---------------------------------------------------------------------------|
      * |          |SHIFT|FN1|SUP|   | ALT|FN0| SHIFT|                           |
      * `---------------------------------------------------------------------------'
      */
     KEYMAP(
-         ESC,  LALT, PSCR, POWER, SLEP, WAKE,          1,    2,    3,    0,    PPLS, BSPC, \
-         FN11, MPLY, MSTP, MEDIA_EJECT, MPRV, MNXT,    4,    5,    6,    PMNS, PAST,  ENT, \
-               FN21, FN22, MUTE, VOLU, VOLD,           7,    8,    9,    PPLS, PEQL,       \
+         ESC,  LALT, PSCR, POWER, SLEP, WAKE,          SLSH,    0,       1,    2,   3, BSPC, \
+         FN11, MPLY, MSTP, MEDIA_EJECT, MPRV, MNXT,    PEQL,    PMNS,    4,    5,   6,  ENT, \
+               FN21, FN22, MUTE, VOLU, VOLD,           PAST,    PPLS,    7,    8,   9,       \
                LSFT, FN1,  LGUI,                       RALT, FN0,  RSFT                    ),
 
     /* 3: FN 1
      * ,-------------------------------------------------------------------.
-     * |ESC   |  /|  ||  \|  (|  )|     _|     ,|     .|     ?|  !|    Bspc|
+     * |ESC   |  &|  (|  )|  $|  <|     "|     _|     -|     ?|  !|    Bspc|
      * |-------------------------------------------------------------------|
-     * |TAB   |  `|  '|  "|  [|  ]|  LEFT|  DOWN|    UP| RIGHT|  <|  Return|
+     * |TAB   |  :|  [|  ]|  %|  ^|     '|  LEFT|  DOWN|  UP|RIGHT|  Return|
      * |-------------------------------------------------------------------|
-     * |      |  #|  ~|  &|  {|  }|     @|     ^|     $|     %|  >|        |
+     * |      |  ;|  {|  }|  `|  >|     #|     @|     ||     ~|  \|        |
      * |-------------------------------------------------------------------|
-     * |          |SHIFT|FN1|SUP|   | ALT|FN0| SHIFT|                   |
+     * |          |SHIFT|FN1|SUP|   | ALT|FN0| SHIFT|                      |
      * `-------------------------------------------------------------------'
      */
     KEYMAP(
-         ESC, SLSH,  NUBS,  FN2,  FN3,  FN4,  FN5,  COMM,  DOT,  FN6,  FN7, BSPC, \
-         FN11,  GRV, QUOT,  FN8, LBRC, RBRC, LEFT, DOWN,   UP, RIGHT,  FN9,  ENT, \
-              NUHS, FN10, FN12, FN13, FN14, FN15, FN16, FN17,  FN18, FN19,       \
+         ESC, FN12,  FN3,  FN4,  FN17,  FN9,  FN15,  FN5,  MINS,  FN6,  FN7, BSPC, \
+         FN11,FN20, LBRC,  RBRC, FN18, FN16, QUOT, LEFT,  DOWN,   UP,RIGHT,  ENT, \
+              SCLN, FN13,  FN14,  GRV, FN19, FN11, FN8,   FN2, FN10, BSLS,       \
                 LSFT,FN1, LGUI,           RALT,FN0,RSFT                ),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
  [0] = ACTION_LAYER_TAP_KEY(2, KC_SPACE),            // LAYER 0
  [1] = ACTION_LAYER_TAP_KEY(3, KC_SPACE),            // LAYER 1
- [2] = ACTION_MODS_KEY(MOD_LSFT, KC_NUBS),           // pipe
+ [2] = ACTION_MODS_KEY(MOD_LSFT, KC_BSLS),           // pipe
  [3] = ACTION_MODS_KEY(MOD_LSFT, KC_9),              // right bracket
  [4] = ACTION_MODS_KEY(MOD_LSFT, KC_0),              // left bracket
  [5] = ACTION_MODS_KEY(MOD_LSFT, KC_MINS),           // underscore
@@ -82,8 +82,8 @@ const uint16_t PROGMEM fn_actions[] = {
  [7] = ACTION_MODS_KEY(MOD_LSFT, KC_1),              // exclamation
  [8] = ACTION_MODS_KEY(MOD_LSFT, KC_2),              // double quote
  [9] = ACTION_MODS_KEY(MOD_LSFT, KC_COMM),           // left angle bracket
- [10] = ACTION_MODS_KEY(MOD_LSFT, KC_NUHS),          // tilda
- [11] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_TAB),       // CTRL and TAB
+ [10] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),           // tilda
+ [11] = ACTION_MODS_KEY(MOD_LSFT, KC_3),             // hash
  [12] = ACTION_MODS_KEY(MOD_LSFT, KC_7),             // ampersand
  [13] = ACTION_MODS_KEY(MOD_LSFT, KC_LBRC),          // left curly braces
  [14] = ACTION_MODS_KEY(MOD_LSFT, KC_RBRC),          // right curly braces
@@ -92,7 +92,7 @@ const uint16_t PROGMEM fn_actions[] = {
  [17] = ACTION_MODS_KEY(MOD_LSFT, KC_4),             // dollar
  [18] = ACTION_MODS_KEY(MOD_LSFT, KC_5),             // percentage
  [19] = ACTION_MODS_KEY(MOD_LSFT, KC_DOT),           // right angle bracket
- [20] = ACTION_MODS_KEY(MOD_LSFT|MOD_RSFT, KC_PAUS), // software reset
+ [20] = ACTION_MODS_KEY(MOD_LSFT, KC_SCLN),          // colon
  [21] = ACTION_DEFAULT_LAYER_SET(0),                 // set qwerty layout
  [22] = ACTION_DEFAULT_LAYER_SET(1),                 // set dvorak layout
 };
